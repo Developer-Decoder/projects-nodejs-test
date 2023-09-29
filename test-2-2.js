494f75ce0eace8e7601d4227c7e1bff205e2f6af
Developer-Decoder <developerdecoder2@gmail.com>
2023-09-29 12:31:49 +0330

    deleteed all file project and creaet new project

--- test-2-2.js
+++
@@ -1,9 +0,0 @@
-//CommonJS , every file is module (by default)
-//Modules - Encapsulated Code (only share minimum)
-
-const sayHi = (name) => {
-  console.log(`Hello ${name}`);
-};
-
-//export default
-module.exports = sayHi;
--- test-2-5.js
+++
@@ -1,8 +0,0 @@
-const num_1 = 5;
-const num_2 = 10;
-
-function addValues() {
-  console.log(`this sum is : ${num_1 + num_2}`);
-}
-
-addValues();
